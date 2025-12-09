546924b472344e815c22c891b800387b3f4d41f4
Khaled Habib <khaled.habib18@gmail.com>
2025-12-09 22:13:24 +0200

    adding delete exam endpoint

--- routes/examRoute.js
+++ routes/examRoute.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const { authorize } = require('../middlewares/auth');
 const createExam = require('../controllers/createExam')
 const getExams = require('../controllers/getExams');
+const deleteExam = require('../controllers/deleteExam');
 examRoutes.use(express.json());
 examRoutes.use(cors());
 
 
 examRoutes.post("/create-exam", authorize('teacher'), createExam);
 examRoutes.get("/get-exams", authorize('student', 'teacher'), getExams);
+examRoutes.delete("/delete-exam", authorize('teacher'), deleteExam);
 module.exports = examRoutes;
